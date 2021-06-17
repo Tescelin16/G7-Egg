c1fa8b50efc58375b2167e8ce312c793f8ce1a38
Pablo Ibañez <pablomat91@gmail.com>
2021-06-17 20:09:31 -0300

    Agencia

--- src/main/java/turismoreceptivo/web/controller/AgenciaController.java
+++ src/main/java/turismoreceptivo/web/controller/AgenciaController.java
@@ -3,6 +3,7 @@ package turismoreceptivo.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,4 +46,24 @@ public class AgenciaController{
         return new RedirectView("/Agencia");
     }
     
+    @GetMapping("/editar/{legajo}")
+    public ModelAndView editarAgencia(@PathVariable Integer legajo){
+        ModelAndView mav = new ModelAndView("agencia-formulario");
+        mav.addObject("agencia", agenciaService.buscarPorLegajo(legajo));
+        mav.addObject("title", "Editar Agencia");
+        mav.addObject("action", "modificar");
+        return mav;
+    }
+    
+    @PostMapping("/modificar")
+    public RedirectView modificar(@RequestParam Integer legajo, @RequestParam String nombre, @RequestParam String telefono, @RequestParam String direccion, @RequestParam String email){
+        agenciaService.modificar(legajo, nombre, telefono, direccion, email);
+        return new RedirectView("/Agencia");
+    }
+    
+    @PostMapping("/eliminar/{legajo}")
+    public RedirectView eliminar(@PathVariable Integer legajo){
+        agenciaService.eliminar(legajo);
+        return new RedirectView("/Agencia");
+    }
 }
